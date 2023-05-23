d43f04376e208d1565d670e50629f62c5e6bd060
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-05-23 10:36:21 +0200

    Second commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+const getToDos = () => {
+  fetch(apiUrl + '?_limit=5')
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((todo) => addToDoToDOM(todo));
+    });
+};
+
+const addToDoToDOM = (todo) => {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute('data-id', todo.id);
+
+  if (todo.completed) {
+    div.classList.add('done');
+  }
+
+  document.getElementById('todo-list').appendChild(div);
+};
+
+const createTodo = (e) => {
+  e.preventDefault();
+
+  const newTodo = {
+    title: e.target.firstElementChild.value,
+    completed: false,
+  };
+
+  fetch(apiUrl, {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => addToDoToDOM(data));
+};
+
+const init = () => {
+  document.addEventListener('DOMContentLoaded', getToDos);
+  document.querySelector('#todo-form').addEventListener('submit', createTodo);
+};
+
+init();
